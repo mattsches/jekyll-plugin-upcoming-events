07a9e3702ccaa94465eb032b33c65362b517e62c
Matthias Gutjahr <mattsches@gmail.com>
2016-02-25 00:09:21 +0100

    Simplify stuff

--- current_event.rb
+++ current_event.rb
@@ -1,21 +1,12 @@
 module Reading
   class Generator < Jekyll::Generator
-    VERSION = '0.0.2'
+    VERSION = '0.1.0'
 
     def generate(site)
       site.collections.each do |name, collection|
         if name == 'events'
-          current_event = find_current_event(collection)
-          site.data['currentEvent'] = current_event
+          site.data['currentEvent'] = collection.docs.select { |event| event.data['start'] > Time.now }.first
         end
-      end
-    end
-
-    def find_current_event(events)
-      current_time = Time.now.strftime('%s')
-      events.docs.each do |event|
-        event_start = event.data['start'].strftime('%s')
-        return event if (event_start > current_time)
       end
     end
 
