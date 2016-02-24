6896a471d2ffbc979e5a582f23537dbf3a8df3ac
Matthias Gutjahr <mattsches@gmail.com>
2016-02-24 13:46:32 +0100

    Fix coding style

--- current_event.rb
+++ current_event.rb
@@ -1,22 +1,21 @@
 module Reading
   class Generator < Jekyll::Generator
-    VERSION = '0.0.1'
+    VERSION = '0.0.2'
 
     def generate(site)
       site.collections.each do |name, collection|
         if name == 'events'
-          currentEvent = findCurrentEvent(collection)
-          # Jekyll.logger.info "Current event is #{currentEvent.data['title']} on #{currentEvent.data['start']}"
-          site.data['currentEvent'] = currentEvent
+          current_event = find_current_event(collection)
+          site.data['currentEvent'] = current_event
         end
       end
     end
 
-    def findCurrentEvent(events)
-      currentTime = Time.now.strftime('%s')
+    def find_current_event(events)
+      current_time = Time.now.strftime('%s')
       events.docs.each do |event|
-        eventStart = event.data['start'].strftime('%s')
-        return event if (eventStart > currentTime)
+        event_start = event.data['start'].strftime('%s')
+        return event if (event_start > current_time)
       end
     end
 
