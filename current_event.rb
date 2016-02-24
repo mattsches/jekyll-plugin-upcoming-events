16737fbff94a2980a41f51fd7b146ac1c7d3ccd7
Matthias Gutjahr <mattsches@gmail.com>
2016-02-24 13:02:19 +0100

    Initial commit

---
+++ current_event.rb
@@ -0,0 +1,24 @@
+module Reading
+  class Generator < Jekyll::Generator
+    VERSION = '0.0.1'
+
+    def generate(site)
+      site.collections.each do |name, collection|
+        if name == 'events'
+	      currentEvent = findCurrentEvent(collection)
+	      # Jekyll.logger.info "Current event is #{currentEvent.data['title']} on #{currentEvent.data['start']}"
+	      site.data['currentEvent'] = currentEvent
+        end
+      end
+    end
+
+    def findCurrentEvent(events)
+	  currentTime = Time.now.strftime('%s')
+	  events.docs.each do |event|
+	    eventStart = event.data['start'].strftime('%s')
+	    return event if (eventStart > currentTime)
+	  end
+    end
+
+  end
+end
